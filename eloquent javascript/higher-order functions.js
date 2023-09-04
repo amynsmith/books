bfed63f6cb8f46cfa1059b46f6c2db9f8d34c50e
amynsmith <amynsmith@users.noreply.github.com>
2023-09-04 16:51:42 +0800

    chapter5: higher-order functions

---
+++ eloquent javascript/higher-order functions.js
@@ -0,0 +1,51 @@
+const SCRIPTS = require('./scripts.js');
+
+// flattening
+let arrays = [[1, 2, 3], [4, 5], [6]];
+console.log(arrays.flat())
+let res = arrays.reduce((x,y)=> x.concat(y), [])
+console.log(res)
+
+// your own loop
+function loop(x, func1, func2, func3){
+    while(func1(x)){
+        func3(x);
+        x=func2(x);
+    }
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
+
+// everything
+function every(array, test){
+    return (array.filter(test)).length === array.length;
+}
+
+console.log(every([1, 3, 5], n => n < 10));
+console.log(every([2, 4, 16], n => n < 10));
+console.log(every([], n => n < 10));
+
+// dominant writing direction
+require('./scripts.js');
+
+function charDirection(code){
+    for (let script of SCRIPTS){
+        inrange = script.ranges.some(([from,to])=>{return code>=from && code < to;})
+        if(inrange){return script.direction;}
+    }
+    return null;
+}
+
+function dominantDirection(text){  
+    direcdict={};
+    for (let t of text){
+        direc = charDirection(t.codePointAt(0));
+        if(!direcdict[direc]){direcdict[direc]=0;}
+        direcdict[direc]++;
+    }
+    ind=Object.values(direcdict).indexOf(Math.max(...Object.values(direcdict)));
+    return Object.keys(direcdict).at(ind)
+}
+
+console.log(dominantDirection("Hello!"));
+console.log(dominantDirection("Hey, مساء الخير"));
